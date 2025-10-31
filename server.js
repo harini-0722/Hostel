cb708764e5f3ca54a8a9239f05defd479ad56b64
Harini <your_email@example.com>
2025-10-31 18:37:59 +0530

    last commit

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static("public")); // Serve your login.html etc.
+app.use(express.static(path.join(__dirname, "public")));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+ // Serve your login.html etc.
 
 // 🚀 --- Multer Image Upload Configuration ---
 const storage = multer.diskStorage({
